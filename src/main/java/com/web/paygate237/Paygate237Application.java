cab503753b3ab4185d8e31657a97c3d79a571225
Enow Enow <dg.danielboaz@ayahoo.com>
2023-03-27 13:34:32 +0100

    initial commit with postgresql

---
+++ src/main/java/com/web/paygate237/Paygate237Application.java
@@ -0,0 +1,13 @@
+package com.web.paygate237;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Paygate237Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Paygate237Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/web/paygate237/Paygate237ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.paygate237;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Paygate237ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
