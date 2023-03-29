7cc8584294ecd5e234eaf5f4a82e1da175988e66
Enow Enow <dg.danielboaz@ayahoo.com>
2023-03-29 16:52:04 +0100

    added user role, user service and security configurations

---
+++ src/main/java/com/web/paygate237/controllers/UserController.java
@@ -0,0 +1,14 @@
+package com.web.paygate237.controllers;
+
+import com.web.paygate237.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+public class UserController {
+
+    //    public String register() {}
+
+}
--- src/main/java/com/web/paygate237/models/User.java
+++ src/main/java/com/web/paygate237/models/User.java
@@ -1,10 +1,22 @@
 package com.web.paygate237.models;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "users")
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -14,57 +26,55 @@ public class User {
     private String email;
     @Column(nullable = false, length = 64)
     private String password;
-    @Column(nullable = false, length = 9)
+    @Column(nullable = false, unique = true, length = 9)
     private int phoneNumber;
 
-    public User() {
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Column(nullable = true,  name = "verification_code", length = 6)
+    private String verificationCode;
+
+    private boolean enabled;
+
+    public User(User user) {
     }
 
-    public User(Long id, String username, String email, String password, int phoneNumber) {
-        this.id = id;
-        this.username = username;
-        this.email = email;
-        this.password = password;
-        this.phoneNumber = phoneNumber;
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
     }
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
+    @Override
     public String getPassword() {
         return password;
     }
 
-    public void setPassword(String password) {
-        this.password = password;
+    @Override
+    public String getUsername() {
+        return email;
     }
 
-    public int getPhoneNumber() {
-        return phoneNumber;
+    public String getFullPhoneNumber() {
+        return "+237"+phoneNumber;
+    }
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
     }
 
-    public void setPhoneNumber(int phoneNumber) {
-        this.phoneNumber = phoneNumber;
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return enabled;
     }
 }
